618ed30ba407adffe9f7b140be38b5307b54b622
Shrawan-git <shrawanshaky3@gmail.com>
2021-02-02 15:40:59 +0545

    products posted and card view for home page

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,10 @@ import Signin from "./user/Signin";
 import Home from "./core/Home";
 import PrivateRoute from "./auth/PrivateRoute";
 import Dashboard from "./user/UserDashboard";
+import AdminRoute from "./auth/AdminRoute";
+import AdminDashboard from "./user/AdminDashboard";
+import AddCategory from "./admin/AddCategory";
+import AddProduct from "./admin/AddProduct";
 
 const Routes = () => {
   return (
@@ -13,7 +17,12 @@ const Routes = () => {
         <Route path="/" exact component={Home} />
         <Route path="/signin" exact component={Signin} />
         <Route path="/signup" exact component={Signup} />
+
         <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
+
+        <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
+        <AdminRoute path="/create/category" exact component={AddCategory} />
+        <AdminRoute path="/create/product" exact component={AddProduct} />
       </Switch>
     </BrowserRouter>
   );
